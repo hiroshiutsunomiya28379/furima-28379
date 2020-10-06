a724ea88e4b4db85c2485d664d19342bdffafc3e
hiroshiutsunomiya <68364020+hiroshiutsunomiya@users.noreply.github.com>
2020-10-06 11:21:34 +0900

    共通の正規表現を変数化

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "Include both letters and numbers"}
+  FULL_WIDTH_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  FULL_WIDTH_KANA_REGEX = /\A[ァ-ヶー－]+\z/
   with_options presence: true do
     validates :nickname
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Full-width characters"}
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Full-width katakana characters"}
+    validates :first_name, :last_name, format: { with: FULL_WIDTH_REGEX, message: "Full-width characters"}
+    validates :first_name_kana, :last_name_kana, format: { with: FULL_WIDTH_KANA_REGEX, message: "Full-width katakana characters"}
     validates :birthday
   end
 end
