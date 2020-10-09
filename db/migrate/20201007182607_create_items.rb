18f76e71f9c256d84b3d9c45649533f1ed463bc7
hiroshiutsunomiya <68364020+hiroshiutsunomiya@users.noreply.github.com>
2020-10-09 23:53:45 +0900

    itemsテーブルの作成

---
+++ db/migrate/20201007182607_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.references :user, null: false,  foreign_key: true
+      t.integer :price, index: true, null: false
+      t.string :name, null: false
+      t.text :description, null: false
+      t.integer :category_id, null: false
+      t.integer :status_id, null: false
+      t.integer :ship_charge_id, null: false
+      t.integer :ship_region_id, null: false
+      t.integer :ship_day_id, null: false
+      t.timestamps
+    end
+  end
+end
