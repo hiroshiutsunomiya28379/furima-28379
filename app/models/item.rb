2ec1720228884cff3dbb90dfc3aa5a1dfb484a05
hiroshiutsunomiya <68364020+hiroshiutsunomiya@users.noreply.github.com>
2020-10-12 22:04:36 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image, :name, :description
+    validates :price, format: {with: /\A[0-9]+\z/, message: "Half-width number"}
+  end
+  validates :category_id, :status_id, :ship_charge_id, :ship_region_id, :ship_day_id, numericality: {other_than: 0, message: "Select"}
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
 end
--- db/migrate/20201007182607_create_items.rb
+++ db/migrate/20201007182607_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.references :user, null: false,  foreign_key: true
+      t.references :user,            null: false,  foreign_key: true
       t.integer :price, index: true, null: false
-      t.string :name, null: false
-      t.text :description, null: false
-      t.integer :category_id, null: false
-      t.integer :status_id, null: false
-      t.integer :ship_charge_id, null: false
-      t.integer :ship_region_id, null: false
-      t.integer :ship_day_id, null: false
+      t.string :name,                null: false
+      t.text :description,           null: false
+      t.integer :category_id,        null: false
+      t.integer :status_id,          null: false
+      t.integer :ship_charge_id,     null: false
+      t.integer :ship_region_id,     null: false
+      t.integer :ship_day_id,        null: false
       t.timestamps
     end
   end
