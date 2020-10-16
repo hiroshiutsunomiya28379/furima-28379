f0262e680219c3a0578a0aa8548db873112e72c1
hiroshiutsunomiya <68364020+hiroshiutsunomiya@users.noreply.github.com>
2020-10-16 12:56:39 +0900

    商品投稿機能に関する単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,9 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
-    validates :image, :name, :description
-    validates :price, format: {with: /\A[0-9]+\z/, message: "Half-width number"}
+    validates :image, :name, :description, :price
   end
   validates :category_id, :status_id, :ship_charge_id, :ship_region_id, :ship_day_id, numericality: {other_than: 0, message: "Select"}
+  validates :price, numericality: { only_integer: true, message: "Half-width number"}
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,85 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+
+    it '全ての値が正しく入力されていれば保存できること' do
+      expect(@item).to be_valid
+    end
+
+    it 'imageが空では保存できないこと' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+
+    it 'nameが空では保存できないこと' do
+      @item.name = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'descriptionが空では保存できないこと' do
+      @item.description = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Description can't be blank")
+    end
+
+    it 'category_idを選択していないと保存できないこと' do
+      @item.category_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category Select")
+    end
+
+    it 'status_idを選択していないと保存できないこと' do
+      @item.status_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Status Select")
+    end
+
+    it 'ship_charge_idを選択していないと保存できないこと' do
+      @item.ship_charge_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Ship charge Select")
+    end
+
+    it 'ship_region_idを選択していないと保存できないこと' do
+      @item.ship_region_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Ship region Select")
+    end
+
+    it 'ship_day_idを選択しないないと保存できないこと' do
+      @item.ship_day_id = 0
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Ship day Select")
+    end
+
+    it 'priceが空だと保存できいないこと' do
+      @item.price = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it 'priceが全角数字だと保存できないこと' do
+      @item.price = '２０００'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price Half-width number")
+    end
+
+    it 'priceが300円未満では保存できないこと' do
+      @item.price = 299
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price Out of setting range")
+    end
+
+    it 'priceが9,999,999円を超過すると保存できないこと' do
+      @item.price = 10000000
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price Out of setting range")
+    end
+  end
 end
