b4513162670386cb316d458bec4232d0cb4b241f
hiroshiutsunomiya <68364020+hiroshiutsunomiya@users.noreply.github.com>
2020-10-16 14:23:08 +0900

    javascriptに関する不備の修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,9 +3,9 @@ function price (){
   inputPrice.addEventListener("input", () => {
     const inputValue = document.getElementById("item-price").value;
     const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = `${Math.floor(inputValue * 0.1)}円`;
+    addTaxDom.innerHTML = `${Math.floor(inputValue * 0.1)}`;
     const profitDom = document.getElementById("profit");
-    profitDom.innerHTML = `${Math.floor(inputValue * 0.9)}円`;
+    profitDom.innerHTML = `${Math.floor(inputValue * 0.9)}`;
   });
 }
 
