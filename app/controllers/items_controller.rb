96432bb39a7dded7ccf1d2452f3fc6ccc247e2ca
hiroshiutsunomiya <68364020+hiroshiutsunomiya@users.noreply.github.com>
2020-10-18 13:28:05 +0900

    ビューファイルの配置

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: :show 
+  before_action :set_item, only: [:show, :edit]
   
   def index
     @items = Item.all.order("created_at DESC")
@@ -17,6 +17,9 @@ class ItemsController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def edit
   end
 
   def show
