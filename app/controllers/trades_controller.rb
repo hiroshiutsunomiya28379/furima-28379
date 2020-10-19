18621f2de5708904d3460b20e6dbc87754c17456
hiroshiutsunomiya <68364020+hiroshiutsunomiya@users.noreply.github.com>
2020-10-20 07:25:42 +0900

    ルーティングとコントローラーの設定

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -2,4 +2,14 @@ class TradesController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
   end
+
+  def create
+    Address.create(address_params)
+  end
+
+  private
+
+  def address_params
+    params.permit(:postal_code, :prefecture, :city, :house_number, :building_name, :phone_number)
+  end
 end
