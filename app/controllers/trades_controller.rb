6e97ab94fca88285fe0ae6cb31406a1c7f08feb9
hiroshiutsunomiya <68364020+hiroshiutsunomiya@users.noreply.github.com>
2020-10-23 12:25:54 +0900

    controllerにカード決済処理の記述を追加

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -9,12 +9,27 @@ class TradesController < ApplicationController
 
   def create
     @trade = UserTrade.new(trade_params)
-    @trade.save
+    if @trade.valid?
+      pay_item
+      @trade.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
   end
 
   private
 
   def trade_params
-    params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :trade_id, :user_id, :item_id)
+    params.permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :trade_id, :user_id, :item_id).merge(token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ""
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: trade_params[:token],
+      currency: 'jpy'
+    )
   end
 end
--- app/models/user_trade.rb
+++ app/models/user_trade.rb
@@ -1,10 +1,10 @@
 class UserTrade
 
   include ActiveModel::Model 
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :trade_id
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :trade_id, :token
 
   with_options presence: true do
-    validates :city, :house_number 
+    validates :city, :house_number, :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"}
     validates :prefecture_id, numericality: { other_than: 0, message: "Select"}
     validates :phone_number, numericality: { only_integer: true, message: "Input only number"}
