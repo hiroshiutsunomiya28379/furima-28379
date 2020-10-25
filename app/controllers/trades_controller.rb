e6bfa1a1a2d366bd0053b655c806fff740fbeaf0
hiroshiutsunomiya <68364020+hiroshiutsunomiya@users.noreply.github.com>
2020-10-25 16:39:25 +0900

    conttollerのbefore_actionの順番を修正

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,7 +1,7 @@
 class TradesController < ApplicationController
   before_action :authenticate_user!
-  before_action :move_to_index
   before_action :set_item
+  before_action :move_to_index
 
   def index
     @user_trade = UserTrade.new
@@ -35,7 +35,6 @@ class TradesController < ApplicationController
   end
 
   def move_to_index
-    @item = Item.find(params[:item_id])
     if current_user.id == @item.user_id || @item.trade != nil
       redirect_to root_path
     end
